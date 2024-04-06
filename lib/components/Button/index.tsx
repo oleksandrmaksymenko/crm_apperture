8e6308c6ebeee5342613522ca4135884839e1f88
Oleksandr <sky3107@gmail.com>
2024-04-06 23:00:22 +0300

    add variant button

--- lib/components/Button/index.tsx
+++ lib/components/Button/index.tsx
@@ -1,10 +1,12 @@
 export type ButtonProps = {
   color?: "blue" | "red" | "green";
+  variant?: "primary" | "secondary" | "tertiary";
   children: React.ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>
 
-const Button: React.FC<ButtonProps> = ({ color = "blue", children, ...rest }) => {
+const Button: React.FC<ButtonProps> = ({ color = "blue", children, variant, ...rest }) => {
   let colorClasses = "bg-blue-500";
+  let variantClasses = "primary";
 
   if (color === "red") {
     colorClasses = "bg-red-500";
@@ -12,9 +14,13 @@ const Button: React.FC<ButtonProps> = ({ color = "blue", children, ...rest }) =>
     colorClasses = "bg-green-500";
   }
 
+  if (variant === "secondary") {
+    variantClasses = "secondary";
+  }
+
   return (
     <button
-      className={`${colorClasses} text-xl font-medium text-white transition-colors px-6 py-2 rounded-md`}
+      className={`${colorClasses} ${variantClasses} text-xl font-medium text-white transition-colors px-6 py-2 rounded-md`}
       {...rest}
     >
       {children}
