0422250e19eb4782b7529b8ca8bbcdec1562d2f9
Oleksandr <sky3107@gmail.com>
2024-04-13 11:58:06 +0300

    add button group to export

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,7 @@
 export {default as Button} from './components/Button';
 export {default as Popup} from './components/Popup';
 export {default as Paper} from './components/Paper';
+export {default as ButtonGroup} from './components/ButtonGroup';
 
 export {default as useClickOutside} from './hooks/useClickOutside';
 
